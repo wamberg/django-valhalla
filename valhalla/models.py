d37f2988ec1fde6b38f339119a99bf1c3ac321d3
William Amberg <wamberg@technushi.com>
2008-11-19 22:59:43 -0500

    Added a ForeignKey to the django.contrib.auth.models.User because we'll need an authenticated user to create Deeds in the system.

--- valhalla/models.py
+++ valhalla/models.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+
+from django.contrib.auth import models as auth_models
 from django.db import models
+
 
 class Deed(models.Model):
     """
     Who said what when as reported by someone.
+
+    ``text`` - what is to be recorded
+    ``speaker`` - who said the ``text``
+    ``witness`` - who recorded what the ``speaker`` said
+    ``report_date`` - when the Deed was recorded
+    ``reporter`` - the authenticated user responsible for creating the db record
     """
     text = models.TextField()
     speaker = models.CharField(max_length=200)
-    reporter = models.CharField(max_length=200)
-    report_date = models.DateTimeField()
+    witness = models.CharField(max_length=200)
+    report_date = models.DateTimeField(default=datetime.now)
+    reporter = models.ForeignKey(auth_models.User)
