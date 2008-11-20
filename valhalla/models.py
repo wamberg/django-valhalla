3ac01e9599eabb6c89bb20a66587071bef46f071
William Amberg <wamberg@technushi.com>
2008-11-19 22:35:30 -0500

    Added first swath of Deed model.

--- valhalla/models.py
+++ valhalla/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Deed(models.Model):
+    """
+    Who said what when as reported by someone.
+    """
+    text = models.TextField()
+    speaker = models.CharField(max_length=200)
+    reporter = models.CharField(max_length=200)
+    report_date = models.DateTimeField()
