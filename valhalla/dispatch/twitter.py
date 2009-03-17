8f48eb2ac990fcb6f01c670ba2e21be4e9b63f14
wamberg <wamberg@wasp.(none)>
2009-03-16 21:49:54 -0400

    Pull valhalla twitter dispatch settings from settings.py.

--- valhalla/dispatch/twitter.py
+++ valhalla/dispatch/twitter.py
@@ -1,8 +1,20 @@
 
 from sys import modules
+from django.conf import settings
+from django.core import exceptions
 from python_twitter import Api
 
 class TwitterDispatcher(object):
     def send(self, message):
-        api = Api(username='crewtons', password='XXXXX')
+        try:
+            username = settings.VALHALLA_TWITTER_USERNAME
+        except AttributeError:
+            raise exceptions.ImproperlyConfigured(
+                    'You must set a VALHALLA_TWITTER_USERNAME in settings.py.')
+        try:
+            password = settings.VALHALLA_TWITTER_PASSWORD
+        except AttributeError:
+            raise exceptions.ImproperlyConfigured(
+                    'You must set a VALHALLA_TWITTER_PASSWORD in settings.py.')
+        api = Api(username=username, password=password)
         api.PostUpdate(message)
